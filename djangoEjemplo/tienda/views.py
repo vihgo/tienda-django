589a291776e65fce927edeebc12c3857ef4ee0ee
unknown <Laboratorio@MPAPCA03LC0100.duoc.acad>
2024-06-04 15:13:03 -0400

    cambios para agregar comentarios a la pagina

--- djangoEjemplo/tienda/views.py
+++ djangoEjemplo/tienda/views.py
@@ -6,4 +6,12 @@ def index(request):
     juegos= Juego.objects.filter(categoria=1).values()
     context={"RPG":juegos}
     return render(request,'tienda/index.html',context)
+
+def juego(request):
+    idx=request.GET['juegoId']
+    print(idx)
+    juego= Juego.objects.get(id=idx)
+    context={"juego":juego}
+    return render(request,'tienda/juego.html', context)
+    
     
