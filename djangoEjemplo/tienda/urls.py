c4d907c217a1a87b873a2168b3b1e052638c76eb
vihgo <vi.gomeza@profesor.duoc.cl>
2024-06-11 01:03:27 -0400

    se comleto el crud de comentarios

--- djangoEjemplo/tienda/urls.py
+++ djangoEjemplo/tienda/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('juego',views.juego,name='Juego'),
-    path('juego/<int:comentario_pk>',views.comentarioDelete,name="comentario_delete")
+    path('juego',views.juego,name='juego'),
+    path('comentario_delete/<int:comentario_pk>',views.comentarioDelete,name="comentario_delete"),
+    path('comentario_edit/<int:comentario_pk>',views.comentario,name="comentario"),
+    path('comentario_edit/comentario_update',views.comentarioUpdate,name="comentario_update"),
 ]
 #<!--{% url 'comentarioDelete' comentario_pk=comentario.id juego_pk=juego.id %}-->
--- djangoEjemplo/tienda/views.py
+++ djangoEjemplo/tienda/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Juego,Comentario
 
 # Create your views here.
@@ -7,14 +7,28 @@ def index(request):
     context={"RPG":juegos}
     return render(request,'tienda/index.html',context)
 
+def comentario(request,comentario_pk):
+    comentario=Comentario.objects.get(id=comentario_pk)
+    juego=comentario.juego
+    context={"juego":juego, "comentario":comentario}
+    return render(request,'tienda/comentario.html',context)
+
+def comentarioUpdate(request):
+    comentario=Comentario.objects.get(id=request.POST["comentario_id"])
+    comentario.nombre=request.POST["nombre"]
+    comentario.email=request.POST["email"]
+    comentario.comentario=request.POST["comentario"]
+    comentario.save()
+    context={"juego":comentario.juego, "comentario":comentario}
+    return render(request,'tienda/comentario.html',context)
+
+
+
 def comentarioDelete(request,comentario_pk):
 
     comentario=Comentario.objects.get(id=comentario_pk)
-    juego=comentario.juego
     comentario.delete() 
-    comentarios=Comentario.objects.filter(juego=juego)
-    context={"juego":juego, "comentarios":comentarios}
-    return render(request,'tienda/juego.html',context=context)
+    return render(request,'tienda/comentario.html')
 
 
     
